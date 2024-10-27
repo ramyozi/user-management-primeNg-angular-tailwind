5e5be28c561145cfded32cb4330634df855d6b89
Ramzi <ramzibenmansour.pro@gmail.com>
2024-10-27 16:17:46 +0100

    setup Ripple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PrimeNGConfig} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
   title = 'user-management';
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
