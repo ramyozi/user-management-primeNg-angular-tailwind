3bcceff38dc9d4f742d5c45f34783d451118411f
Ramzi <ramzibenmansour.pro@gmail.com>
2024-10-27 17:19:24 +0100

    Start using ngx-translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,36 @@
 import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {PrimeNGConfig} from 'primeng/api';
-
+import {TranslateService, TranslateModule} from '@ngx-translate/core';
+import { DropdownModule } from 'primeng/dropdown';
+interface LanguageOption {
+  name: string;
+  code: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule, DropdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
 
-  constructor(private primengConfig: PrimeNGConfig) {}
+  languages: LanguageOption[] = [
+    { name: 'English', code: 'en' },
+    { name: 'French', code: 'fr' },
+  ];
+
+  constructor(private primengConfig: PrimeNGConfig, private translateService: TranslateService) {}
 
   title = 'user-management';
   ngOnInit() {
     this.primengConfig.ripple = true;
+    this.translateService.setDefaultLang('fr');
+  }
+
+  translateText(lang: string) {
+    this.translateService.use(lang);
+    this.translateService.get('primeng').subscribe(res => this.primengConfig.setTranslation(res));
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,33 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {HttpClient, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 import { routes } from './app.routes';
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+        },
+        defaultLanguage: 'fr'
+      })
+    ]),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi())
+  ],
 };
